459dc330926df3b479d40b013a02138c20288717
igorl <igor110795@gmail.com>
2020-06-12 16:54:04 +0300

    Prepare to review #2

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -5,7 +5,9 @@ from .locators import BasketPageLocators
 class BasketPage(BasePage):
 
     def should_be_empty_basket(self):
-        assert self.is_element_present(*BasketPageLocators.CONTINUE_SHOPPING)
+        assert self.is_element_present(*BasketPageLocators.CONTINUE_SHOPPING), \
+            "The basket isn't empty."
 
     def should_be_text_about_empty_basket(self):
-        assert self.is_element_present(*BasketPageLocators.EMPTY_TEXT)
+        assert self.is_element_present(*BasketPageLocators.EMPTY_TEXT), \
+            "The text about empty basket isn't presented"
